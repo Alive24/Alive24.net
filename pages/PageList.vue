0d76da5931aa12d4365fd0fd6aaf3f10b6a5ed12
Alive24 <xct24@live.com>
2023-01-28 12:54:11 +0800

    Update PageList.vue

--- pages/PageList.vue
+++ pages/PageList.vue
@@ -8,13 +8,18 @@ const { data: PagesNavigation } = await useAsyncData('navigation', () => fetchCo
         <div v-for="page in PagesNavigation[0].children" :key="page.title" class="card w-96 bg-base-100 shadow-xl">
             <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
             <div class="card-body">
-                <ContentDoc v-slot="{ doc }"   :path="`${page._path}`">
-                    <h2 class="prose-2xl font-bold max-w-none flex justify-between gap-2 items-center">
-                        {{ doc.title }}
-                        <!-- <span class="prose-sm lg:prose-xl font-light w-40 text-right " >{{ doc.head.meta.find((item) => item.name == "date").content }}</span> -->
-                    </h2>
+                <ContentDoc v-slot="{ doc }" :path="`${page._path}`">
+                    <NuxtLink :to="`/Content?type=pages&title=${doc.title}`">
+                        <h2 class="prose-2xl font-bold max-w-none flex justify-between gap-2 items-center">
+                            {{ doc.title }}
+                            <!-- <span class="prose-sm lg:prose-xl font-light w-40 text-right " >{{ doc.head.meta.find((item) => item.name == "date").content }}</span> -->
+                        </h2>
+                    </NuxtLink>
+
                     <ContentRenderer class="prose max-w-none prose-sm" excerpt :value="doc" />
-                    <div class="absolute inset-x-0 px-3 bottom-0 divider">阅读更多</div>
+                    <NuxtLink :to="`/Content?type=pages&title=${doc.title}`">
+                        <div class="absolute inset-x-0 px-3 bottom-0 divider">阅读更多</div>
+                    </NuxtLink>
                 </ContentDoc>
             </div>
         </div>
