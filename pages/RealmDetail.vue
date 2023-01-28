29c87283487119c745ab9d66e59d237de23b1491
Alive24 <xct24@live.com>
2023-01-29 06:25:53 +0800

    Update

--- pages/RealmDetail.vue
+++ pages/RealmDetail.vue
@@ -1,22 +1,21 @@
 <template>
     <div>
         <div v-for="realmContent in realmContentList" :key="realmContent.title">
-            <div class="container mx-auto py-10 px-5 xl:max-w-7xl"
+            <div class="container mx-auto py-10 px-3 xl:max-w-7xl"
                 :class="{ hidden: contentTitle != realmContent.title }">
                 <div class="card lg:card-side bg-base-100 shadow-xl ">
-                    <figure>
-                        <img class="h-full" src="/image/Shimakaze-Yuubari.png" alt="Album" />
-                        <!-- ToDo: Width should be relatively fixed -->
+                    <figure class="lg:w-2/5">
+                        <img class="h-full" :src="`${realmContent.realmHero}`" alt="Album" />
                     </figure>
-                    <div class="card-body w-5/6">
+                    <div class="card-body lg:w-5/6">
                         <h2 class="card-title">{{ realmContent.realmNameCN }}</h2>
                         <p class="prose max-w-none whitespace-pre-wrap lg:h-64 flex flex-wrap">{{
                             realmContent.realmDescriptionCN
                         }}</p>
                         <div class="card-actions justify-start">
-                            <div class="hidden h-72 w-10/12 lg:flex gap-y-2 gap-x-2 flex-wrap w-5/6 bottom-0">
+                            <div class="hidden h-72 w-10/12 lg:flex gap-y-2 gap-x-2 flex-wrap w-5/6">
                                 <div v-for="category in realmContent.body" :key="category.categoryName"
-                                    class="bottom-0 card rounded-box place-items-start">
+                                    class="card rounded-box place-items-start">
                                     <h3 class="prose text-left font-bold">{{ category.categoryNameCN }}</h3>
                                     <div class="gap-y-2 gap-x-2 flex flex-wrap w-full ">
                                         <NuxtLink v-for="item in category.items" :key="item.itemName"
@@ -33,6 +32,7 @@
                 </div>
                 <div v-for="category in realmContent.body" :key="category.categoryName"
                     class="card my-5 bg-base-100 shadow-xl">
+                    <!-- ToDo: Switch to tab style on mobile to increase view width -->
                     <div class="card-body">
                         <h2 :id='`${category.categoryName}`' class="card-title">{{ category.categoryNameCN }}</h2>
                         <div v-for="item in category.items" :key="item.itemName" tabindex="0"
